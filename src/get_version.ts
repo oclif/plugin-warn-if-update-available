cc872eaf93ae66812374dceb6a5362ce1e2fcfdb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-23 18:46:22 -0700

    fix: prevent multiple updates

--- src/get_version.ts
+++ src/get_version.ts
@@ -1,16 +1,8 @@
 import * as fs from 'fs-extra'
 import HTTP from 'http-call'
 
-async function touch(file: string) {
-  if (!await fs.pathExists(file)) {
-    await fs.outputJSON(file, {})
-  } else {
-    await fs.utimes(file, new Date(), new Date())
-  }
-}
-
 async function run(name: string, file: string, version: string) {
-  await touch(file)
+  await fs.outputJSON(file, {current: version}) // touch file with current version to prevent multiple updates
   const {body} = await HTTP.get(`https://registry.npmjs.org/${name.replace('/', '%2f')}`, {timeout: 5000})
   await fs.outputJSON(file, {...body['dist-tags'], current: version})
   process.exit(0)
